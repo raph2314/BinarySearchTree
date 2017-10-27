1d86328aadb0b919bc1b929ec2112d3d923e9d1a
raph2314 <rwlopez@edu.uwaterloo.ca>
2017-10-26 21:38:27 -0400

    removed unnecessary test cases

--- BST/a3_main.cpp
+++ BST/a3_main.cpp
@@ -11,33 +11,7 @@ string get_status_str(bool status)
 
 int main()
 {
-    PriorityQueueTest pqueue_test;
     BinarySearchTreeTest bst_test;
-    AVLTreeTest avl_test;
-
-	string pqueue_test_descriptions[6] = {
-      "Test1: New queue is valid",
-      "Test2: Enqueue one item and then dequeue it",
-      "Test3: Enqueue too many",
-      "Test4: Enqueue too many then dequeue too many",
-      "Test5: Dequeue 5, enqueue 10, with repeat",
-      "Test6: Lots of enqueues and dequeues"
-	};
-	
-	bool pqueue_test_results[6];
-    pqueue_test_results[0] = pqueue_test.test1();
-    pqueue_test_results[1] = pqueue_test.test2();
-    pqueue_test_results[2] = pqueue_test.test3();
-    pqueue_test_results[3] = pqueue_test.test4();
-    pqueue_test_results[4] = pqueue_test.test5();
-    pqueue_test_results[5] = pqueue_test.test6();
-
-	cout << "PRIORITY QUEUE TESTING RESULTS \n";
-	cout << "****************************** \n";
-	for (int i = 0; i < 6; ++i) {
-    	cout << pqueue_test_descriptions[i] << endl << get_status_str(pqueue_test_results[i]) << endl;
-	}
-	cout << endl;
     
 	string bst_test_descriptions[8] = {
       "Test1: New tree is valid",
@@ -64,31 +38,6 @@ int main()
 	cout << "********************************** \n";
 	for (int i = 0; i < 8; ++i) {
     	cout << bst_test_descriptions[i] << endl << get_status_str(bst_test_results[i]) << endl;
-	}
-	cout << endl;
-	
-	string avl_test_descriptions[6] = {
-      "Test1: Test single left rotation",
-      "Test2: Test single right rotation",
-      "Test3: Test double left-right rotation",
-      "Test4: Test double right-left rotation",
-      "Test5: Test multiple rotations on insert",
-      "Test6: Test multiple rotations on remove"
-	};
-    
-    bool avl_test_results[6];
-    avl_test_results[0] = avl_test.test1();
-    avl_test_results[1] = avl_test.test2();
-    avl_test_results[2] = avl_test.test3();
-    avl_test_results[3] = avl_test.test4();
-    avl_test_results[4] = avl_test.test5();
-    avl_test_results[5] = avl_test.test6();
-    
-
-	cout << "AVL TREE TESTING RESULTS \n";
-	cout << "************************ \n";
-	for (int i = 0; i < 6; ++i) {
-    	cout << avl_test_descriptions[i] << endl << get_status_str(avl_test_results[i]) << endl;
 	}
 	cout << endl;
 
